353a08c2a70f2872f9e5d1db7570938d934df836
Ishani <ishaniamin@gmail.com>
2018-06-19 00:30:36 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -6,27 +6,3 @@ end
 def eur_to_usd(money)
   money * 1.16
 end
-
-def usd_to_jpy(money)
-  money * 110.15
-end
-
-def jpy_to_usd(money)
-  money * 0.0091
-end
-
-def usd_to_gbp(money)
-  momey * 0.75
-end
-
-def gbp_to_usd(money)
-  momey * 1.33
-end
-
-def usd_to_aud(money)
-  momey * 1.35
-end
-
-def aud_to_usd(money)
-  money * 0.74
-end
