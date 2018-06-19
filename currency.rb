1ca7271720993d3d38f08d2a6edee3ff85d8224f
Ishani <ishaniamin@gmail.com>
2018-06-19 00:44:54 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -49,19 +49,19 @@ choice = gets.chomp().to_i
 
 case choice
 when 1
-  # puts usd_to_eur(amount)
+  puts usd_to_eur(amount)
 when 2
-  # puts usd_to_jpy(amount)
+  puts usd_to_jpy(amount)
 when 4
-  # puts usd_to_gbp(amount)
+  puts usd_to_gbp(amount)
 when 5
-  # puts usd_to_aud(amount)
+  puts usd_to_aud(amount)
 when 6
-  # puts eur_to_usd(amount)
+  puts eur_to_usd(amount)
 when 7
-  # puts gbp_to_usd(amount)
+  puts gbp_to_usd(amount)
 when 8
-  # puts aud_to_usd(amount)
+  puts aud_to_usd(amount)
 else
   puts "Invalid input, exiting..."
 end
